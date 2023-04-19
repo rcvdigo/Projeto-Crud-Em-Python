177baf8faaf462046834b618b6402a1e3c8bbc46
rcvdigo <131033913+rcvdigo@users.noreply.github.com>
2023-04-19 17:53:36 -0300

    delete

--- appCrud/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppcrudConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appCrud'
--- appCrud/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from appCrud.models import Carros
-
-# Create the form class.
-class CarrosForm(ModelForm):
-    class Meta:
-        model = Carros
-        fields = ["modelo", "marca", "ano"]
