8ccc14130d750a47e900241da07a97238d90acba
Rodrigo Camurça Vera <131033913+rcvdigo@users.noreply.github.com>
2024-02-01 14:51:55 -0300

    Update settings.py

--- projetoCrud/settings.py
+++ projetoCrud/settings.py
@@ -15,7 +15,7 @@ import os
 from pathlib import Path
 import dj_database_url
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'projetoCrud.urls'
