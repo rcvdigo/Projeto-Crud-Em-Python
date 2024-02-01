8c9de9e4df07e327cb7ae842c3f9a01088223462
Rodrigo Camurça Vera <131033913+rcvdigo@users.noreply.github.com>
2024-02-01 14:43:40 -0300

    Update settings.py

--- projetoCrud/settings.py
+++ projetoCrud/settings.py
@@ -15,6 +15,8 @@ import os
 from pathlib import Path
 import dj_database_url
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
