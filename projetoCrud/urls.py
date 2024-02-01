2760675c96f665bf37333d7698f1327d735696f6
Rodrigo Camurça Vera <131033913+rcvdigo@users.noreply.github.com>
2024-02-01 14:38:41 -0300

    Update urls.py

--- projetoCrud/urls.py
+++ projetoCrud/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.decorators.csrf import get_token
 from appCrud.views import home, form, create, view, edit, update, delete
 
 urlpatterns = [
@@ -28,6 +29,7 @@ urlpatterns = [
     path('edit/<int:pk>/', edit, name = 'edit'),
     path('update/<int:pk>/', update, name = 'update'),
     path('delete/<int:pk>/', delete, name = 'delete'),
+    path('csrf_token/', get_token, name='csrf_token'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
