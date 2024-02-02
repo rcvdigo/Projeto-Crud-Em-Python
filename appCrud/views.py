04dbac5df7ec72e8d21e9f0e545d64240dc7aadd
Rodrigo Camurça Vera <131033913+rcvdigo@users.noreply.github.com>
2024-02-02 15:58:06 -0300

    Update views.py

--- appCrud/views.py
+++ appCrud/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework import status
 from appCrud.forms import CarrosForm
 from appCrud.models import Carros
 
@@ -30,122 +30,30 @@ def home(request):
         data['db'] = paginator.get_page(pages)
     return render(request, 'index.html', data)
 
+
 def form(request):
     data ={}
     data['form'] = CarrosForm()
     return render(request, 'form.html', data)
-    
 
-@api_view(['POST'])
+
 @csrf_exempt
 def create(request):
     if request.method == 'POST':
-        # Verifica se a requisição é JSON
-        if 'application/json' in request.content_type:
-            data = request.data
-            form = CarrosForm(data)
-        else:
-            form = CarrosForm(request.POST)
-        
-        if form.is_valid():
-            car = form.save()
-            return Response(
-                {
-                    'message': 'Carro criado com sucesso',
-                    'post_man': data,
-                    'html': request.POST
-                }, status=status.HTTP_201_CREATED)
-        else:
-            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+        # if request.headers.get('content-type') == 'application/json':  # Verifica se a requisição é JSON
+        # Se a requisição é JSON, carrega os dados do corpo da requisição
+        data = request.POST.dict()
+        form = CarrosForm(data)
+        # else:
+        # Se a requisição é via formulário HTML, usa request.POST diretamente
+        form = CarrosForm(request.POST)
 
-
-
-
-
-# @csrf_exempt
-# def create(request):
-#     if request.method == 'POST':
-#         # if request.headers.get('content-type') == 'application/json':  # Verifica se a requisição é JSON
-#         # Se a requisição é JSON, carrega os dados do corpo da requisição
-#         data = request.POST.dict()
-#         form = CarrosForm(data)
-#         # else:
-#         # Se a requisição é via formulário HTML, usa request.POST diretamente
-#         form = CarrosForm(request.POST)
-
-#         return JsonResponse(
-#             {
-#                 'postman': request.POST.dict(),
-#                 'form_html': request.POST
-#             }
-#         )
-
-
-#     if form.is_valid():
-#         try:
-#             car = form.save()
-#             if request.is_ajax() or request.headers.get('content-type') == 'application/json':
-#                 # Se a requisição veio via AJAX ou é JSON, retorna JSON com o ID do carro
-#                 return JsonResponse({'car_id': car.id}, status=201)
-#             else:
-#                 # Se a requisição veio via formulário HTML, redireciona para uma página de sucesso
-#                 # Adapte o redirecionamento conforme necessário
-#                 return JsonResponse({'message': 'Carro criado com sucesso'}, status=201)
-#         except ValueError as e:
-#             return JsonResponse({'error_message': str(e)}, status=400)
-#     else:
-#         return JsonResponse({'error_message': 'Formulário inválido'}, status=400)
-# else:
-#     return JsonResponse({'error_message': 'Apenas solicitações POST são permitidas'}, status=405)
-
-
-
-
-
-
-# def create(request):
-#     if request.method == 'POST':
-#         form = CarrosForm(request.POST)
-#         form_type = type(form).__name__
-#         if form.is_valid():
-#             try:
-#                 car = form.save()
-#                 if request.is_ajax():
-#                     return JsonResponse({'car_id': car.id}, status=201)
-#                 else:
-#                     return JsonResponse({'message': 'Carro criado com sucesso'}, status=201)
-#             except ValueError as e:
-#                 return JsonResponse({'error_message': str(e)}, status=400)
-#         else:
-#             return JsonResponse({'error_message': 'Formulário inválido'}, status=400)
-#     else:
-#         return JsonResponse({'error_message': 'Apenas solicitações POST são permitidas'}, status=405)
-
-
-
-
-
-# form = CarrosForm(request.POST)
-# form_type = type(form).__name__
-# return JsonResponse(
-#     {
-#         'request': request.POST
-#     }, status=201
-# )
-
-
-# if form.is_valid():
-#     try:
-#         form.save()
-#         return JsonResponse(
-#             {
-#                 'request': request.POST
-#             }, status=201
-#         )
-#     except ValueError as e:
-#         # Aqui você pode lidar com a exceção ValueError como achar adequado
-#         return JsonResponse({'error_message': str(e)}, status=400)
-        # redirect('home')
+        return JsonResponse(
+            {
+                'postman': request.POST.dict(),
+                'form_html': request.POST
+            }
+        )
 
 def view(request, pk):
     data = {}
