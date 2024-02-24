0945399d79e341a47e3f4f3e52acd851aaf110b2
rcvdigo <rcvdigo@gmail.com>
2024-02-24 19:46:20 -0300

    feat: Alterando o banco de dados para SQLite

--- projetoCrud/settings.py
+++ projetoCrud/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'projetoCrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': { # dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {  # dj_database_url.parse(os.environ.get("DATABASE_URL"))
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
